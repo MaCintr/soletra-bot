66a23b9b924acdae1be64d9f1569dd8faee76c62
MaCintr <marcelocintra2307@gmail.com>
2025-10-24 10:15:13 -0300

    Ajustes

--- scraper/game_reader.py
+++ scraper/game_reader.py
@@ -16,7 +16,7 @@ def ler_elementos_da_pagina():
     print("---------------- Iniciando jogo ----------------")
     time.sleep(1)
     driver.find_element(By.CSS_SELECTOR, ".button.button--game-white.drawer-close-icon.svelte-1t84pcu").click()
-    print("---------------- Fechando instruções ----------------")
+    print("------------- Fechando instruções --------------")
     time.sleep(1)
     
 
@@ -36,11 +36,14 @@ def ler_elementos_da_pagina():
     print(f"Palavras válidas para o desafio ({len(palavras_validas)})=> ", palavras_validas)
     
     actions.move_to_element(driver.find_element(By.TAG_NAME, "body")).click().perform()
+    time.sleep(1)
     
     index = 1
 
     for palavra in palavras_validas:
         print(f"Testando palavra {index} de {len(palavras_validas)}=> ", palavra)
+        actions.move_to_element(driver.find_element(By.TAG_NAME, "body")).click().perform()
+        time.sleep(0.2)
         index += 1
         for letra in palavra:
             actions.send_keys(letra).perform()
@@ -50,7 +53,7 @@ def ler_elementos_da_pagina():
     
     print("Lista de palavras percorrida com sucesso!")
     time.sleep(3)
-    print("Encerrando partida")
+    print("-------------- Encerrando partida --------------")
     botao_encerrar = driver.find_element(By.CSS_SELECTOR, 'button[title="Botão responsável por encerrar o jogo"]')
     botao_encerrar.click()
     time.sleep(30)
